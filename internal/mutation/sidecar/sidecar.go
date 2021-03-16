08f88e52e92bbe2d41342b59153e440612997a7c
d-hayashi <hayashi.daiki@nagoya-u.jp>
2021-03-16 20:25:09 +0900

    Support executing ssh in init-container

--- internal/mutation/sidecar/sidecar.go
+++ internal/mutation/sidecar/sidecar.go
@@ -42,39 +42,97 @@ func (s sidecarinjector) Inject(_ context.Context, obj metav1.Object) error {
 	}
 
 	// Check labels
+	sshUser := ""
+	sshHost := ""
+	sshPort := "22"
 	isInjection := false
 	for key, value := range labels {
 		if key == "k8s-slurm-injector/injection" && value == "enabled" {
 			isInjection = true
 		}
+		if key == "k8s-slurm-injector/ssh-user" {
+			sshUser = value
+		}
+		if key == "k8s-slurm-injector/ssh-host" {
+			sshHost = value
+		}
+		if key == "k8s-slurm-injector/ssh-port" {
+			sshPort = value
+		}
 	}
 	if !isInjection {
 		return nil
 	}
+	if sshUser == "" {
+		return fmt.Errorf("label 'k8s-slurm-injector/ssh-user' is required")
+	}
+	if sshHost == "" {
+		return fmt.Errorf("label 'k8s-slurm-injector/ssh-host' is required")
+	}
+
+	sshDestination := sshUser + "@" + sshHost
 
 	// Add init-container
 	initContainer := corev1.Container{
-		Name:            "initial-container",
-		Image:           "busybox",
-		Args:            []string{"sleep", "10"},
+		Name:  "initial-container",
+		Image: "dhayashi/k8s-slurm-injector:ssh-client",
+		Command: []string{
+			"ssh",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-i",
+			"/root/.ssh/id_rsa",
+			sshDestination,
+			"-p",
+			sshPort,
+		},
+		Args:            []string{"squeue"},
 		ImagePullPolicy: "IfNotPresent",
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "k8s-slurm-injector-ssh-id-rsa",
+				MountPath: "/root/.ssh/id_rsa",
+				SubPath:   "id_rsa",
+				ReadOnly:  true,
+			},
+		},
+	}
+	mode := int32(256) // 400 = (4 * 8^2) + (4 * 8^1) + (4 * 8^0) = (4 * 64) + (0 * 8) + (0 * 1) = 256 + 0 + 0 = 256
+	mode1 := &mode
+	volume := corev1.Volume{
+		Name: "k8s-slurm-injector-ssh-id-rsa",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName:  "k8s-slurm-injector-ssh-id-rsa",
+				DefaultMode: mode1,
+			},
+		},
 	}
 
 	switch v := obj.(type) {
 	case *corev1.Pod:
-		v.Spec.InitContainers = append(v.Spec.InitContainers, initContainer)
+		v.Spec.InitContainers = append([]corev1.Container{initContainer}, v.Spec.InitContainers...)
+		v.Spec.Volumes = append([]corev1.Volume{volume}, v.Spec.Volumes...)
 	case *batchv1.Job:
-		v.Spec.Template.Spec.InitContainers = append(v.Spec.Template.Spec.InitContainers, initContainer)
+		v.Spec.Template.Spec.InitContainers =
+			append([]corev1.Container{initContainer}, v.Spec.Template.Spec.InitContainers...)
+		v.Spec.Template.Spec.Volumes = append([]corev1.Volume{volume}, v.Spec.Template.Spec.Volumes...)
 	case *batchv1beta1.CronJob:
 		v.Spec.JobTemplate.Spec.Template.Spec.InitContainers =
-			append(v.Spec.JobTemplate.Spec.Template.Spec.InitContainers, initContainer)
+			append([]corev1.Container{initContainer}, v.Spec.JobTemplate.Spec.Template.Spec.InitContainers...)
+		v.Spec.JobTemplate.Spec.Template.Spec.Volumes =
+			append([]corev1.Volume{volume}, v.Spec.JobTemplate.Spec.Template.Spec.Volumes...)
 	default:
-		return fmt.Errorf("not implemented")
+		return fmt.Errorf("unsupported type")
 	}
 
-	// Modify labels
-	labels["k8s-slurm-injector/status"] = "injected"
-	obj.SetLabels(labels)
+	// Modify annotations
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+	annotations["k8s-slurm-injector/status"] = "injected"
+	obj.SetAnnotations(annotations)
 
 	return nil
 }
